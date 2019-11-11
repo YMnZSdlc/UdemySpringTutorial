c2b2ae17f323752f4edea2f446c9ceeba6f1d446
YMnZSdlc <michal.mielczarek@gmail.com>
2019-11-11 16:09:29 +0100

    test upgrade

--- src/test/java/pl/ymz/udemy/spring/HelloServiceTest.java
+++ src/test/java/pl/ymz/udemy/spring/HelloServiceTest.java
@@ -2,30 +2,46 @@ package pl.ymz.udemy.spring;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
 
 public class HelloServiceTest {
-
-    private HelloService SUT = new HelloService(); // System Under Test = SUT
+    private final static String HELLO = "Hello";
 
     @Test
-    public void test_prepareGreeting_null_returnsGreeteingWithFallback() {
-        // given + then
-        var result = SUT.prepareGreeting(null);
+    public void test_prepareGreeting_nullName_returnsGreetingWithFallbackName() {
+        // given
+        var mockRepository = alwaysReturningHelloRepository();
+        var SUT = new HelloService(mockRepository); // System Under Test = SUT
 
         // then
-        assertEquals("Hello " + HelloService.FALLBACK_NAME + "!", result);
+        var result = SUT.prepareGreeting(null, "-1");
+
+        // then
+        assertEquals(HELLO + " " + HelloService.FALLBACK_NAME + "!", result);
     }
 
     @Test
     public void test_prepareGreeting_name_returnsGreetingWitchName() {
         // given
+        var mockRepository = alwaysReturningHelloRepository();
+        var SUT = new HelloService(mockRepository); // System Under Test = SUT
         var name = "TestName";
 
         // then
-        var result = SUT.prepareGreeting(name);
+        var result = SUT.prepareGreeting(name, "-1");
 
         // then
-        assertEquals("Hello " + name + "!", result);
+        assertEquals(HELLO + " " + name + "!", result);
+    }
+
+    private LangRepository alwaysReturningHelloRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Long id) {
+                return Optional.of(new Lang(null, HELLO, null));
+            }
+        };
     }
 }
