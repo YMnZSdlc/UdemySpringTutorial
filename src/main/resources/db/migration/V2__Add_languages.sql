ffe46028048849a8890a3c7058f68137c7841f35
YMnZSdlc <michal.mielczarek@gmail.com>
2019-11-16 17:43:50 +0100

    Hibernate config

---
+++ src/main/resources/db/migration/V2__Add_languages.sql
@@ -0,0 +1,22 @@
+insert into languages (welcomeMsg, code)
+values ('Hello', 'en');
+insert into languages (welcomeMsg, code)
+values ('Witaj', 'pl');
+insert into languages (welcomeMsg, code)
+values ('Priviet', 'ru');
+insert into languages (welcomeMsg, code)
+values ('Bonjur', 'fr');
+insert into languages (welcomeMsg, code)
+values ('HailiHailoHaila', 'de');
+insert into languages (welcomeMsg, code)
+values ('Welcome 01', 'en1');
+insert into languages (welcomeMsg, code)
+values ('Welcome 02', 'en2');
+insert into languages (welcomeMsg, code)
+values ('Welcome 03', 'en3');
+insert into languages (welcomeMsg, code)
+values ('Welcome 04', 'en4');
+insert into languages (welcomeMsg, code)
+values ('Welcome 05', 'en5');
+insert into languages (welcomeMsg, code)
+values ('Welcome 06', 'en6');
