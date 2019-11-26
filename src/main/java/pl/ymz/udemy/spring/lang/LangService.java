2035f57a38c80e02b8827a5e40915a84f21af254
YMnZSdlc <michal.mielczarek@gmail.com>
2019-11-26 22:14:35 +0100

    add LangDTO and LangService

---
+++ src/main/java/pl/ymz/udemy/spring/lang/LangService.java
@@ -0,0 +1,27 @@
+package pl.ymz.udemy.spring.lang;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+class LangService {
+    private LangRepository repository;
+
+    LangService() {
+        this(new LangRepository());
+    }
+
+    LangService(LangRepository repository) {
+        this.repository = repository;
+    }
+
+    List<LangDTO> findAll() {
+        return repository
+                .findAll()
+                .stream()
+                .map(lang -> new LangDTO(lang))
+                .collect(toList());
+    }
+
+
+}
